82448762ed1c139942174b83527913c163a2ac48
Ege <96302110+EgeEken@users.noreply.github.com>
2023-12-18 22:28:26 +0100

    Day 6

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,5 +1,3 @@
-
-
 def FUNCTION(timedist):
     res = 1
     for td in timedist:
@@ -7,19 +5,13 @@ def FUNCTION(timedist):
         for i in range(1, td[0]):
             if i * (td[0] - i) > td[1]:
                 temp += 1
-        print(temp)
         res *= temp
     return res
-            
-
+    
 fr = open("input.txt", "r")
 lines = fr.readlines()
 time = [int(i) for i in lines[0][:-1].split(": ")[1].split(" ") if i.isnumeric()]
 dist = [int(i) for i in lines[1][:-1].split(": ")[1].split(" ") if i.isnumeric()]
 timedist = list(zip(time, dist))
-
-print(time)
-print(dist)
-print(timedist)
 
 print(FUNCTION(timedist))
